671ec45e46b45c7a5958150e41014904767430ca
Greg Malkov <51164901+gmalkov@users.noreply.github.com>
2020-09-21 13:31:36 -0400

    DDO-683 Pull preview env config into main terra-env module with flags (#6)

--- actions/preview/terraform/main.tf
+++ actions/preview/terraform/main.tf
@@ -1,9 +1,10 @@
 module "env" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-preview-env?ref=terra-preview-env-0.0.0"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-env?ref=master"
 
   providers = {
     google.target      = google
     google.dns         = google.dns
+    google-beta.target = google-beta
   }
 
   google_project = var.google_project
@@ -11,9 +12,13 @@ module "env" {
   cluster_short  = var.cluster_short
   owner          = var.owner
 
-  dns_zone_name = var.dns_zone_name
+  env_type = "preview"
 
   terra_apps = var.terra_apps
+
+  dns_zone_name  = var.dns_zone_name
+  subdomain_name = local.subdomain_name
+  use_subdomain  = true
 
   versions = var.versions
 }
--- actions/preview/terraform/terraform.tf
+++ actions/preview/terraform/terraform.tf
@@ -3,6 +3,11 @@ provider "google" {
   region  = var.region
 }
 
+provider "google-beta" {
+  project = var.google_project
+  region  = var.region
+}
+
 provider "google" {
   alias   = "dns"
   project = var.dns_google_project
