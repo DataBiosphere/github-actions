be052af9909d78d8b196679675cd7c76955e4519
Greg Malkov <51164901+gmalkov@users.noreply.github.com>
2020-09-22 12:55:13 -0400

    DDO-605 Add action for tagging actions on merge to master (#7)

---
+++ actions/action-releaser/README.md
@@ -0,0 +1,47 @@
+# chart-releaser
+
+A Github Action to automatically bump and tag master, on merge, with new versions for any changed GitHub actions defined in a repository.
+
+### Usage
+
+```Dockerfile
+name: Release Actions
+on:
+  push:
+    branches:
+      - master
+    paths:
+      - 'actions/**'
+jobs:
+  release:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Bump version and push tags
+      uses: databiosphere/github-actions/actions/action-releaser@master
+      env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+#### Options
+
+**Environment Variables**
+
+* **GITHUB_TOKEN** ***(required)*** - Required for permission to tag the repo
+* **ACTIONS_DIR** *(optional)* - Directory where action subdirectories are located (default: `actions`)
+* **VERSION_BUMP_LEVEL** *(optional)* - Which type of bump to use when none explicitly provided (default: `minor`)
+* **VERBOSITY** *(optional)* - Verbosity level, with 1 being silent and 6 being debug (default: `4`)
+* **GIT_BRANCH** *(optional)* - Branch to look for changes in (default: `master`)
+* **GIT_USER** *(optional)* - User name to initialize Git with (default: `broadbot`)
+* **GIT_EMAIL** *(optional)* - User email to initialize Git with (default: `broadbot@broadinstitute.org`)
+* **GITHUB_OWNER** *(optional)* - GitHub repo owner (default: `DataBiosphere`)
+* **GITHUB_REPO** *(optional)* - GitHub repo name (default: `github-actions`)
+
+### Workflow
+
+* Add this action to your repo
+* Commit some changes
+* Either push to master or open a PR
+* On push (or merge) to `master`, the action will:
+  * Find any actions that were changed in the last commit
+  * For each of those actions, find their latest version tag and bump the version according to `VERSION_BUMP_LEVEL`
+  * Push tags to GitHub
---
+++ actions/action-releaser/entrypoint.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Set variables from their all-caps versions, taking defaults from config file
+set_vars() {
+    local config_file=$1
+    local vars=$(yq r "$1" --printMode p '*')
+    readarray -t varsArr <<< "$vars"
+    for var in "${varsArr[@]}"; do
+        varCaps="${var^^}"
+        if [ -z ${!varCaps+x} ]; then
+            eval "$var"=$(yq r "$config_file" "$var.default")
+        else
+            eval "$var"="${!varCaps}"
+        fi
+    done
+}
+
+main() {
+    git_init
+
+    git clone --single-branch --branch "$git_branch" "https://$github_token@github.com/$github_owner/$github_repo"
+    pushd "$github_repo" > /dev/null
+
+    einfo "Discovering changed actions ..."
+    local changed_actions=()
+    readarray -t changed_actions <<< "$(lookup_changed_actions)"
+
+    if [[ -n "${changed_actions[*]}" ]]; then
+        for action in "${changed_actions[@]}"; do
+            if [[ -d "$actions_dir/$action" ]]; then
+                local current_tag=$(lookup_latest_tag $action)
+                if [[ "$current_tag" == '' ]]; then
+                    local new_semver="0.0.0"
+                else
+                    local current_semver=${current_tag#"$action-"}
+                    local new_semver=$(semver bump $version_bump_level $current_semver)
+                fi
+                tag_action "$action" "$new_semver"
+            else
+                ewarn "Action '$action' no longer exists in repo. Skipping it..."
+            fi
+        done
+        push_tags
+    else
+        einfo "Nothing to do. No action changes detected."
+    fi
+
+    popd > /dev/null
+}
+
+git_init() {
+    git config --global user.name "$git_user"
+    git config --global user.email "$git_email"
+}
+
+lookup_latest_tag() {
+    local action="$1"
+    git fetch --tags > /dev/null 2>&1
+
+    edebug "Looking for latest tag for $action"
+    git for-each-ref --sort=-taggerdate --count=1 --format '%(refname:lstrip=-1)' "refs/tags/$action-*"
+}
+
+filter_actions() {
+    while read action; do
+        [[ ! -d "$actions_dir/$action" ]] && continue
+        local file="$actions_dir/$action/action.yml"
+        if [[ -f "$file" ]]; then
+            echo $action
+        else
+            ewarn "$file is missing, assuming that '$action' is not a GitHub action. Skipping."
+        fi
+    done
+}
+
+lookup_changed_actions() {
+    #look for changed files in the latest commit
+    local changed_files
+    changed_files=$(git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) -- $actions_dir)
+    cut -d '/' -f '2' <<< "$changed_files" | uniq | filter_actions
+}
+
+tag_action() {
+    local action="$1"
+    local version="$2"
+    local tag="$action-$version"
+
+    einfo "Creating tag '$tag'"
+    git tag "$tag"
+}
+
+push_tags() {
+    einfo 'Pushing tags...'
+    git push --tags
+}
+
+colblk='\033[0;30m' # Black - Regular
+colred='\033[0;31m' # Red
+colgrn='\033[0;32m' # Green
+colylw='\033[0;33m' # Yellow
+colpur='\033[0;35m' # Purple
+colrst='\033[0m'    # Text Reset
+
+### verbosity levels
+silent_lvl=0
+crt_lvl=1
+err_lvl=2
+wrn_lvl=3
+ntf_lvl=4
+inf_lvl=5
+dbg_lvl=6
+
+## esilent prints output even in silent mode
+function esilent () { verb_lvl=$silent_lvl elog "$@" ;}
+function enotify () { verb_lvl=$ntf_lvl elog "$@" ;}
+function eok ()    { verb_lvl=$ntf_lvl elog "SUCCESS - $@" ;}
+function ewarn ()  { verb_lvl=$wrn_lvl elog "${colylw}WARNING${colrst} - $@" ;}
+function einfo ()  { verb_lvl=$inf_lvl elog "${colblk}INFO${colrst} ---- $@" ;}
+function edebug () { verb_lvl=$dbg_lvl elog "${colgrn}DEBUG${colrst} --- $@" ;}
+function eerror () { verb_lvl=$err_lvl elog "${colred}ERROR${colrst} --- $@" ;}
+function ecrit ()  { verb_lvl=$crt_lvl elog "${colpur}FATAL${colrst} --- $@" ;}
+function edumpvar () { for var in $@ ; do edebug "$var=${!var}" ; done }
+function elog() {
+        if [ $verbosity -ge $verb_lvl ]; then
+                datestring=$(date +"%Y-%m-%d %H:%M:%S")
+                echo -e "$datestring - $@" 1>&2
+        fi
+}
+
+pushd /releaser > /dev/null
+set_vars "inputs.yaml"
+main
+popd > /dev/null
