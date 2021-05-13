4fe2e553cd86b381606ce20cea2e907b6894f054
Eno Akpan <eaugusti@broadinstitute.org>
2021-05-13 15:06:35 -0400

    Change entry script

--- actions/terra-k8s-linter/entrypoint.sh
+++ actions/terra-k8s-linter/entrypoint.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
-set -x
+
+# RUN DEFAULT POLICIES AND CUSTOM policies
+# -P /POLICY,/CUSTOM_POLICIES_DIR
+# RUN CUSTOM POLICIES ONLY
+#  -P /CUSTOM_POLICIES_DIR
+# RUN DEFAULT POLICIESE ONLY
+#  -P /POLICY
+# RUN CUSTOM POLICIES BUT DONT PROVIDE DIRECTORY
+# THROW ERROR
 
 if [[ $# -lt 1 ]]; then
   echo "Usage: $0 <manifests_dir> $1 <custom_policies_dir> $2 <ignored_policies_dir> " &2
   exit 1
 fi
 
-echo "test5"
-pwd
 
-policy_args=""
+policy_dirs=""
 
 MANIFESTS_DIR="$1"
 CUSTOM_POLICIES_DIR="$2"
 IGNORE_DEFAULT_POLICY="$3"
 
-# if [ $IGNORE_DEFAULT_POLICY ]
-# then
-#   cmd+=" -i /policy"
-# fi
+if [[ "$IGNORE_DEFAULT_POLICY" == "true" ]]; then
+  if [[ -z "$CUSTOM_POLICIES_DIR" ]]; then
+    echo "Please provide directory where custom policies reside." >&2
+    exit 1
+  else
+    policy_dirs="${CUSTOM_POLICIES_DIR}"
+  fi
+else
+  if [[ -z "$CUSTOM_POLICIES_DIR" ]]; then
+    policy_dirs="/policy"
+  else
+    policy_dirs="/policy,${CUSTOM_POLICIES_DIR}"
+  fi
+fi
 
-# if [ -n $CUSTOM_POLICIES_DIR ]
-# then
-#   if [ ${#policy_args} -gt 0 ]
-#   then
-#     policy_args+=",$CUSTOM_POLICIES_DIR"
-#   else
-#     policy_args+="-p $CUSTOM_POLICIES_DIR"
-#   fi
-# fi
-
-conftest test "${MANIFESTS_DIR}"
+conftest test "${MANIFESTS_DIR}" -p "${policy_dirs}"
